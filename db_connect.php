2573bee0f653a2f2b3d1e9809e9f70a8c6a2b0bc
wrongstopsorryg <teddy.collier@my.rongotai.school.nz>
2026-06-15 14:47:18 +1200

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,9 +1,9 @@
 <?php
 // Database connection settings
 $host 	= "db";          	
-$database = "YOUR_DATABASE";   
+$database = "teddy_db";   
 $username = "root";       
-$password = "ENTER_YOUR_PASSWORD_HERE";   
+$password = "PASSWORD";   
  
 try {
 	$pdo = new PDO("mysql:host=$host;dbname=$database;charset=utf8", $username, $password);
